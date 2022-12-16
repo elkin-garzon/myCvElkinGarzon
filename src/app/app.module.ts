824910b84f48f07197243e040d5d754f52f14bb3
elkin-garzon <elkingarzonyaya@gmail.com>
2022-12-16 13:45:00 -0500

    new secction my cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { StudyComponent } from './study/study.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    StudyComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
