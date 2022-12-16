fad40f781037502dea2fbdcd78a366c7ad5921be
elkin-garzon <elkingarzonyaya@gmail.com>
2022-12-16 13:18:17 -0500

    add styles and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
